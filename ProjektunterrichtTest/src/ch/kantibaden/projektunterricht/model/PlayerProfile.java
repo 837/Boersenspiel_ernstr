cb7a4a4b5c46e1da7d918132f02e2e87c051aa29
Onuriss <fabiostra@hotmail.com>
2014-01-12 13:45:16 +0100

    ctrl+shift+f...

--- ProjektunterrichtTest/src/ch/kantibaden/projektunterricht/model/PlayerProfile.java
+++ ProjektunterrichtTest/src/ch/kantibaden/projektunterricht/model/PlayerProfile.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class PlayerProfile {
-
+	
 	private String name;
 	private String email;
 	private String password;
@@ -13,7 +13,7 @@ public class PlayerProfile {
 	private BigDecimal balance;
 	private ArrayList<ShareContainer> ownedShares;
 	private ArrayList<Transaction> transactions;
-
+	
 	public PlayerProfile(String name, String email, String password, int beginningBalance) {
 		this.name = name;
 		this.email = email;
@@ -23,14 +23,14 @@ public class PlayerProfile {
 		this.ownedShares = new ArrayList<>();
 		this.transactions = new ArrayList<>();
 	}
-
+	
 	/**
 	 * @return the name
 	 */
 	public String getName() {
 		return name;
 	}
-
+	
 	/**
 	 * @param name
 	 *            the name to set
@@ -38,14 +38,14 @@ public class PlayerProfile {
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
 	/**
 	 * @return the email
 	 */
 	public String getEmail() {
 		return email;
 	}
-
+	
 	/**
 	 * @param email
 	 *            the email to set
@@ -53,14 +53,14 @@ public class PlayerProfile {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	
 	/**
 	 * @return the password
 	 */
 	public String getPassword() {
 		return password;
 	}
-
+	
 	/**
 	 * @param password
 	 *            the password to set
@@ -68,52 +68,52 @@ public class PlayerProfile {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
 	/**
 	 * @return the balance
 	 */
 	public BigDecimal getBalance() {
 		return balance;
 	}
-
+	
 	public BigDecimal getTotalShareValue() {
 		BigDecimal totalValue = new BigDecimal(0);
-
+		
 		for (ShareContainer currentShare : ownedShares) {
 			totalValue = totalValue.add(currentShare.getShare().getValue().multiply(new BigDecimal(currentShare.getAmount())));
-
+			
 		}
-
+		
 		return totalValue;
 	}
-
+	
 	/**
 	 * @return the ownedShares
 	 */
 	public ArrayList<ShareContainer> getOwnedShares() {
 		return ownedShares;
 	}
-
+	
 	/**
 	 * @return the ownedSharesByShare
 	 */
 	public int getOwnedSharesByShare(Share share) {
 		for (ShareContainer currentShare : ownedShares) {
 			if (currentShare.getShare().equals(share)) {
-
+				
 				return currentShare.getAmount();
 			}
 		}
 		return 0;
 	}
-
+	
 	/**
 	 * @return the beginningBalance
 	 */
 	public BigDecimal getBeginningBalance() {
 		return beginningBalance;
 	}
-
+	
 	public boolean buy(Share shareToBuy, int amount) {
 		if (shareToBuy.getValue().multiply(new BigDecimal(amount)).compareTo(getBalance()) == 1) {// don't
 																									// have
@@ -122,7 +122,7 @@ public class PlayerProfile {
 			System.out.println("Sorry not enough money! You would need: " + shareToBuy.getValue().multiply(new BigDecimal(amount)));
 			return false;
 		}
-
+		
 		System.out.println(getBalance());
 		// getBalance().subtract(shareToBuy.getValue().multiply(new
 		// BigDecimal(amount))); //DAS GAHT NED,
@@ -131,10 +131,10 @@ public class PlayerProfile {
 																										// GAHT
 																										// 0.o
 		System.out.println(getBalance());
-
+		
 		transactions.add(new Transaction(shareToBuy, amount));
 		System.out.println("Just bought: " + shareToBuy.getName() + "  " + shareToBuy.getValue().multiply(new BigDecimal(amount)));
-
+		
 		for (ShareContainer currentShare : ownedShares) {
 			if (currentShare.getShare() == shareToBuy) {// this works because it
 														// actually is the same
@@ -146,14 +146,14 @@ public class PlayerProfile {
 		ownedShares.add(new ShareContainer(shareToBuy, amount));
 		return true;
 	}
-
+	
 	/* HIER STIMMT ETWAS NOCH NICHT, ES GIBT IMMER TRUE ZURÜCK */
 	public boolean sell(Share shareToSell, int amount) {
 		boolean couldSell = false;
 		int actuallySoldAmount = 0;
-
+		
 		for (ShareContainer currentShare : getOwnedShares()) {
-
+			
 			if (currentShare.getShare() == shareToSell) {// this works because
 															// they reference
 															// the same object
@@ -167,22 +167,22 @@ public class PlayerProfile {
 				break;
 			}
 		}
-
+		
 		if (couldSell) {
 			System.out.println(getBalance());
 			// balance.add(shareToSell.getValue().multiply(new
 			// BigDecimal(actuallySoldAmount))); THIS DOES NOT WORKS
-
+			
 			this.balance = getBalance().add(shareToSell.getValue().multiply(new BigDecimal(actuallySoldAmount))); // THIS
 																													// DOES
 			System.out.println(getBalance());
-
+			
 			transactions.add(new Transaction(shareToSell, -actuallySoldAmount));
-
+			
 			System.out.println("Just sold: " + shareToSell.getName() + "  " + shareToSell.getValue().multiply(new BigDecimal(amount)));
-
+			
 		}
 		return couldSell;
 	}
-
+	
 }
